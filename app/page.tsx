00121afff5765eafedb14b063d6e4b0a2aefc07a
Ariel Serato <Serato.arieli@gmail.com>
2024-11-03 20:01:04 -0300

    fix: update footer

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ import { IconStarFilled } from "@tabler/icons-react";
 import { ShootingStarsAndStarsBackgroundDemo } from "@/components/demos/shooting-stars-demo";
 import LetsMakeThingsHappenSection from "@/components/ui/lets-make-things-happen";
 import Navbar from "@/components/ui/navbar";
+import Footer from "@/components/footer";
 
 const services = [
   {
@@ -341,24 +342,7 @@ export default function Home() {
         <LetsMakeThingsHappenSection />
       </section>
 
-      <footer className="bg-[#fafafa] py-10  px-6 md:px-0 md:mx-auto border-t">
-        <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
-          <h1 className="text-3xl md:text-5xl font-medium ">
-            <span className="text-5xl font-bold tracking-wider">XERATO</span>
-          </h1>
-          <p className="text-left  text-xl  text-gray-500">54-91156569844</p>
-          <p className="text-left  text-xl  text-gray-500">
-            serato.arieli@gmail.com
-          </p>
-        </div>
-
-        <div className="flex md:justify-center gap-x-4 mt-10">
-          © 2025 Xerato. All Rights Reserved.
-          <Link href="/" className="text-blue-500">
-            Privacy Policy
-          </Link>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
